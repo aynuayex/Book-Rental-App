26c41fc3fdadd466f4654bb7a5c412b3f4cd8f13
Ayne Abreham <aynuman19@gmail.com>
2024-08-14 10:05:13 +0300

    finalized the cors config

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -3,7 +3,7 @@ const allowedOrigins = [
   // "http://localhost:3000",
   // "http://192.168.8.3:5173",
   // "http://localhost:5173",
-  "https://book-rental-app-frontend-r4qne3jyk-ayne-abrehams-projects.vercel.app"
+  "https://book-rental-app-frontend.vercel.app"
 ];
 
 module.exports = allowedOrigins;
