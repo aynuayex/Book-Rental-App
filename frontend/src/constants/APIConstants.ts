5515c1d2cfac98cd550b90abe57dbaa72fe6004a
Ayne Abreham <aynuman19@gmail.com>
2024-08-14 10:00:00 +0300

    updated api constant

--- frontend/src/constants/APIConstants.ts
+++ frontend/src/constants/APIConstants.ts
@@ -1,2 +1,2 @@
 // export const API_BASE_URL = "http://localhost:3000"
-export const API_BASE_URL = "https://book-rental-app-backend-7612cugvw-ayne-abrehams-projects.vercel.app/"
+export const API_BASE_URL = "https://book-rental-app-backend-nit6assgx-ayne-abrehams-projects.vercel.app"
